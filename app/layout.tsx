a0581175fddda20070ea0850c5425bc5f8f9efd9
Umar Ahmed <umarahmedse@outlook.com>
2025-08-08 20:20:56 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ['latin'] })
+const myFont = localFont({
+  src: "./fonts/Sohne.ttf",
+});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Jordan App',
-  description: 'A modern way of learning Michif'
-}
+  title: "Jordan App",
+  description: "A modern way of learning Michif",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={myFont.className}>{children}</body>
     </html>
-  )
+  );
 }
