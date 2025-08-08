72fd659167a177b61ed58efb546c1acc16240119
Umar Ahmed <umarahmedse@outlook.com>
2025-08-08 21:43:42 +0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const myFont = localFont({
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Jordan App",
+  title: "Michif",
   description: "A modern way of learning Michif",
 };
 
